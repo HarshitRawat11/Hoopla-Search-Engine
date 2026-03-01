7902fa48fa32faf97339d99dbe9e08e1833fdf4d
Harshit <harshitrawat@outlook.com>
2026-03-02 01:06:52 +0530

    Removed stop words and added Stemming

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,5 +1,8 @@
-from lib.search_utils import load_movies
+from lib.search_utils import load_movies, load_stopwords
 import string
+from nltk.stem import PorterStemmer
+
+stemmer = PorterStemmer()
 
 def clean_text(text):
     text = text.lower()
@@ -8,8 +11,20 @@ def clean_text(text):
 
 def tokenize_text(text):
     text = clean_text(text)
-    tokens = [token for token in text.split() if token]
-    return tokens
+    stopwords = load_stopwords()
+    res = []
+    # tokens = [token for token in text.split() if token not in stopwords]
+    
+    def _filter(token):
+        if token and token not in stopwords:
+            return True
+        return False
+    for token in text.split():
+        if _filter(token):
+            token = stemmer.stem(token)
+            res.append(token)
+
+    return res
 
 def has_matching_token(query_tokens, movie_tokens):
     for query_token in query_tokens:
